2fe6f0e4ae4b9c26aab1ccd0f2fcf32d4fae03b7
meyta2020 <tmeysner@gmail.com>
2021-03-24 17:31:37 -0500

    upd

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -1,13 +1,13 @@
 import LoginPage from  '../pageobjects/login.page';
+import ProfilePage from '../pageobjects/profile.page'
 
 describe('Auth', () => {
     it('user logs in with valid data', () => {
         LoginPage.open();
-        LoginPage.setLogin();
-        LoginPage.setPassword();
+        LoginPage.setLogin('tx@gmail.com');
+        LoginPage.setPassword('123456');
         LoginPage.clickSubmitButton();
         ProfilePage.isOpen();
-
     });
 });
 
