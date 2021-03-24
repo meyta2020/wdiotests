f7b17f886d24a3a9aa6f4b38193defc4218595de
meyta2020 <tmeysner@gmail.com>
2021-03-24 16:08:14 -0500

    upd

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -1,16 +1,15 @@
 import LoginPage from  '../pageobjects/login.page';
-import SecurePage from '../pageobjects/secure.page';
 
-describe('My Login application', () => {
-    it('should login with valid credentials', () => {
+describe('Auth', () => {
+    it('user logs in with valid data', () => {
         LoginPage.open();
+        LoginPage.setLogin();
+        LoginPage.setPassword();
+        LoginPage.clickSubmitButton();
+        ProfilePage.isOpen();
 
-        LoginPage.login('tomsmith', 'SuperSecretPassword!');
-        expect(SecurePage.flashAlert).toBeExisting();
-        expect(SecurePage.flashAlert).toHaveTextContaining(
-            'You logged into a secure area!');
     });
 });
-//test
+
 
 
